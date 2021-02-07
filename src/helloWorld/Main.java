9780d7e4fc84af2074e9a8b78f131d18c9cd097d
Angela Martin <angela@angelamartin.info>
2021-02-07 16:04:43 -0500

    added user input

--- src/helloWorld/Main.java
+++ src/helloWorld/Main.java
@@ -1,14 +1,25 @@
 package helloWorld;
 //My First Program
+import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, World!"); 
-		System.out.println("My first Java Program!");
+//		System.out.println("Hello, World!"); 
+//		System.out.print("My first Java Program!");
+
+		Scanner input = new Scanner(System.in);
+	    System.out.println("What is your name?");
+	    String firstName = input.nextLine();
+	    System.out.printf("Hello %s!\n", firstName);
+	    System.out.printf("Goodbye %s!\n", firstName);
 
 	}
+	
+
+
+}
 
 	
-}
+
 
 
